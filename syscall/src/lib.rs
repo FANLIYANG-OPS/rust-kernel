acf205a775f445f1b281b4f9c9913bce319f57f7
gary.fan <1779905848@qq.com>
2021-02-19 18:56:36 +0800

    smaug

--- syscall/src/lib.rs
+++ syscall/src/lib.rs
@@ -1,1 +1,12 @@
+extern crate core;
+
+pub use self::number::*;
+pub use self::number::*;
+
+pub mod call;
 pub mod error;
+pub mod number;
+
+#[cfg(not(any(target_os = "none", target_os = "smaug")))]
+#[path = "arch/smaug.rs"]
+mod arch;
